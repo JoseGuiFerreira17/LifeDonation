54ab988ae531aba256b9d1b32f4b7e98477ded12
José Gui <guibsonmouta15@gmail.com>
2020-03-26 21:48:32 -0300

    primeiro commit

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const routes = require('./routes');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+/*
+*Tipos de parametros:
+*
+*Query: parametros nomeados enviados na rota depois de "?" (filtros, paginação) 
+*Route: parametros para identificar recursos
+*Request Body: corpo da requisição, utilizado para criar ou alterar recursos
+*
+*
+*
+*
+*/
+
+
+app.listen(3333);
